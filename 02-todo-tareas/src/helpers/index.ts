ae691c30113da44c8acda64481b110ba2b2d95a7
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2023-06-04 13:12:15 +0200

    feat: delete helpers

--- 02-todo-tareas/src/helpers/index.ts
+++ 02-todo-tareas/src/helpers/index.ts
@@ -1,4 +1,3 @@
-import inquirer from "inquirer";
 import Task from "../models/Task";
 
 export function printTask(index: number, task: Task) {
@@ -7,42 +6,4 @@ export function printTask(index: number, task: Task) {
 
 export function printList(tasks: Map<string, Task>, cb: (index: number, task: Task) => void) {
   Array.from(tasks).map(([key, task], index) => cb(index, task));
-}
-
-
-export async function completeMenu(tasks: Map<string, Task>) {
-  const choices = Array.from(tasks, ([_, value]) => (value)).map((task: Task) => {
-    return {
-      value: task.id,
-      name: task.description,
-      checked: !!task.completadoEn
-    };
-  });
-  console.log('Completar tarea(s)');
-  const {ids} = await inquirer.prompt([
-    {
-      type: 'checkbox',
-      name: 'ids',
-      message: 'Seleccione la tarea a completar: '.yellow,
-      choices,
-    }
-  ]);
-
-  return ids;
-}
-
-export async function createTaskMenu() {
-  const {description} = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'description',
-      validate(value: string) {
-        if (value.length === 0) {
-          return 'Por favor ingrese una descripción: '.red;
-        }
-        return true;
-      },
-      message: 'Descripción de la tarea: '.yellow,
-    }]);
-  return description;
 }
