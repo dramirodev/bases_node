7c854288a0a859a787aeac7dc609a4293adc7c8b
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2023-06-04 13:11:57 +0200

    feat: refactor MenuController.ts to use Menu interface

--- 02-todo-tareas/src/app.ts
+++ 02-todo-tareas/src/app.ts
@@ -1,11 +1,13 @@
 import MenuController from "./controller/MenuController";
 import {DBTaskImpl} from "./services/DBTaskImpl";
+import {TaskMenuImpl} from "./services/TaskMenuImpl";
 import TasksImpl from "./services/TasksImpl";
 
-const instance =TasksImpl.getInstance(new DBTaskImpl());
 
 function main() {
-  const menu = new MenuController(instance);
+  const instance = TasksImpl.getInstance(new DBTaskImpl());
+  const taskMenu = new TaskMenuImpl();
+  const menu = new MenuController(instance, taskMenu);
   menu.showMenu();
 }
 
--- 02-todo-tareas/src/controller/MenuController.ts
+++ 02-todo-tareas/src/controller/MenuController.ts
@@ -1,107 +1,54 @@
 import 'colors';
-import inquirer from 'inquirer';
-import {completeMenu, createTaskMenu} from "../helpers";
+import {Menu} from "../interfaces/Menu";
 import {Tasks} from "../interfaces/Tasks";
 import Task from "../models/Task";
 
-
 export default class MenuController {
 
-  private service: Tasks;
-  private opt: string = '';
+  private readonly tasks: Tasks;
+  private readonly menu: Menu;
 
-  constructor(provider: Tasks) {
-    this.service = provider;
-  }
-
-  private async menu() {
-    console.clear();
-    return inquirer.prompt([
-      {
-        type: 'list',
-        name: 'option',
-        message: '¿Qué desea hacer?'.underline.yellow,
-        choices: [
-          {
-            value: '1',
-            name: `${'1.'.green} Crear tarea`
-          },
-          {
-            value: '2',
-            name: `${'2.'.green} Listar tareas`
-          },
-          {
-            value: '3',
-            name: `${'3.'.green} Listar tareas completadas`
-          },
-          {
-            value: '4',
-            name: `${'4.'.green} Listar tareas pendientes`
-          },
-          {
-            value: '5',
-            name: `${'5.'.green} Completar tarea(s)`
-          },
-          {
-            value: '6',
-            name: `${'6.'.green} Borrar tarea`
-          },
-          {
-            value: '0',
-            name: `${'0.'.green} Salir\n`
-          }
-        ]
-      }]);
-  }
-
-  async pause() {
-    await inquirer.prompt([
-      {
-        message: `Presione ${'ENTER'.green} para continuar`,
-        type: 'input',
-        name: 'enter',
-      }]);
-
+  constructor(service: Tasks, menu: Menu) {
+    this.tasks = service;
+    this.menu = menu;
   }
 
   async manageOption() {
-    switch (this.opt) {
+    switch (this.menu.getOption()) {
       case '1':
-        const description = await createTaskMenu();
+        const description = await this.menu.createTaskMenu();
         const newTask = new Task(description);
-        this.service.add(newTask);
+        this.tasks.add(newTask);
         console.log('Tarea creada correctamente \n'.green);
         break;
       case '2':
-        this.service.list();
+        this.tasks.list();
         break;
       case '3':
-        this.service.listCompleted();
+        this.tasks.listCompleted();
         break;
       case '4':
-        this.service.listPending();
+        this.tasks.listPending();
         break;
       case '5':
-        const ids = await completeMenu(this.service.getTasks());
-        this.service.complete(ids);
+        const ids = await this.menu.completeMenu(this.tasks.getTasks());
+        this.tasks.complete(ids);
         break;
       case '6':
-        console.log('Borrar tarea');
-        // this.service.delete();
+        const id = await this.menu.deleteMenu(this.tasks.getTasks());
+        this.tasks.delete(id);
         break;
       default:
         break;
     }
-    await this.pause();
+    await this.menu.pause();
   }
 
   public async showMenu() {
     do {
-      const {option} = await this.menu();
-      this.opt = option;
+      await this.menu.menu();
       await this.manageOption();
-      // await this.pause();
-    } while (this.opt !== '0');
+    } while (this.menu.getOption() !== '0');
   }
 
 
